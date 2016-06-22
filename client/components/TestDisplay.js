23cfa9249c122f76fa17e43ff2ccaa2416581268
John Jenson <john@Johns-MacBook-Pro-3.local>
2016-06-22 16:10:15 -0700

    Start on front-end mockup

---
+++ client/components/TestDisplay.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+class TestDisplay extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  componentDidMount() {
+
+  }
+
+  render() {
+    return (
+      <h1>Dashboard</h1>
+      //code snippet
+      //tests (map over data)
+      //add test button
+    );
+  }
+}
+
+export default TestDisplay;
