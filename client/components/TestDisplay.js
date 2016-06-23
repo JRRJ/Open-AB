317fba561f6d7a64c75db305e33bfa2f2e19e1a1
John Jenson <john@Johns-MacBook-Pro-3.local>
2016-06-22 20:33:28 -0700

    Give client ability to add tests

--- client/components/TestDisplay.js
+++ client/components/TestDisplay.js
@@ -3,11 +3,11 @@ import React from 'react';
 class TestDisplay extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {tests:[]};
+    this.state = {tests:[], testName: ''};
   }
 
   componentDidMount() {
-    fetch('http://localhost:8080/api/getall')
+    fetch('/api/getall')
     .then((res) => {
       return res.json();
     })
@@ -41,8 +41,25 @@ class TestDisplay extends React.Component {
     </script>`});
   }
 
+  updateTestName(e) {
+    this.setState({testName: e.target.value})
+  }
+
   addTest() {
-    fetch
+    console.log('is it happening?', this)
+    fetch('/api/createtest', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        testName: this.state.testName
+      })
+    })
+    .then((res) => {
+      console.log(res)
+      location.reload();
+    })
   }
 
   render() {
@@ -58,15 +75,21 @@ class TestDisplay extends React.Component {
         <h1>Dashboard</h1>
         <h3>Current Tests</h3>
         <ul>{this.state.tests.map((test, index)=>{
-          return <li key={test.id}>Test {test.id}: {test.result_a} clicks</li>
+          return (<li key={test.id}>
+            <strong>Test {test.id}: {test.result_a} clicks</strong><br/>
+            <span>Description: {test.name}</span><br/>
+            <span>HTML class name: openab-{test.id}</span>
+          </li>)
         }).sort((a,b) => {
           return a.key - b.key;
         })}</ul>
         <p>Copy this code snippet into the &lt;head&gt; of your website's html</p>
         <textarea style = {snippetStyle} value = {this.state.snippet} readonly/>
         <p>If your website does not already use jQuery, copy this snippet above the previous one</p>
-        <textarea style = {jqueryStyle} value = '<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>' readonly/>
-        //add test button
+        <textarea style = {jqueryStyle} defaultValue = '<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>' readonly/>
+        <h3>Add additional tests</h3>
+        <label>short name/description<input value={this.state.testName} onChange={this.updateTestName.bind(this)}/></label>
+        <button onClick={this.addTest.bind(this)}>Add test</button>
       </div>
     );
   }
